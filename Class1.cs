3cda829d479b28ac767a8d458d88ea07c9cfec51
Ash Lee <131741963+notChocoMilk@users.noreply.github.com>
2024-05-19 12:48:45 -0500

    initial source commit

---
+++ Class1.cs
@@ -0,0 +1,65 @@
+﻿// seein' as you downloaded the source code for a mod this simple, you probably need help explaining what all of this does. i left some comments for help :3
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using MelonLoader;
+
+namespace ExampleModThingy
+{
+    public class Class1 : MelonMod
+    {
+        // these are the variables specified at the start of the script. data is assigned to these later.
+        private GameObject player;
+        private healthsystem healthSystem;
+
+        // runs when the game starts
+        public override void OnApplicationStart()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            // this looks for the 'Player' object in the scene when loaded, then assigns it to the variable.
+            player = GameObject.Find("Player");
+
+            /*
+             * if the player turns out to exist,
+             * it grabs the health system on the player (manages the player health)
+             * & assigns it to the 'healthSystem' variable for later usage.
+             * using variables is good practice to save performance, as using gameobject.find every frame isn't a good idea.
+            */
+
+            if (player != null)
+            {
+                healthSystem = player.GetComponent<healthsystem>();
+            }
+        }
+
+        // this is run every frame.. foreverrrrrrr~
+        public override void OnUpdate()
+        {
+            // checks if the player & health system exist
+            if (player != null && healthSystem != null)
+            {
+                // if it makes it to this point, both exist. it then checks for any input.
+                if (Input.anyKeyDown)
+                {
+                    // grabs a random number & assigns it to a variable
+                    int randomNumber = UnityEngine.Random.Range(9, 1001);
+
+                    // if the number is 9 (it can be anything, i just chose it bc it's my favorite :3), it will advance, else it does nothing.
+                    if (randomNumber == 9)
+                    {
+                        // this is the last thing it does! if it makes it to this point, it sets the player health to zero, killing them instantly.
+                        healthSystem.Health = -10;
+                        /*
+                        update here! turns out, there was a bug in the last release. the player wouldn't actually die, as they would just heal back to 5hp instantly!
+                        i've modified it to set the health to -10 to make SURE they're dead. i'm too lazy to recompile it, so the bug will still be there in the release dll on github..
+                        */
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using ExampleModThingy;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("basically russian roulette - yeah, i did make a mod for my own game!")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("russian roulette - THE MOD!!11")]
+[assembly: AssemblyCopyright("Copyright ChocoGames ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: MelonInfo(typeof(Class1), "bootleg russian roulette", "1.0.0", "ChocoGames")] // if you wanna' edit this, the format follows as such: 'mod name', 'version number', 'developer name'.
+[assembly: MelonGame("ChocoGames", "sandboxLabs")] // leave this as such
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7e6e561a-1d0d-4b21-8ad5-e493be7188d9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
